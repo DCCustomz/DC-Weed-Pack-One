e83d25d944950b4d454891ec3edac802fd6a2807
DCCustomz <dkccustoms5m@gmail.com>
2024-07-26 10:01:09 -0400

    Add files via upload

---
+++ dc_weedpackone/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "cerulean"
+games { "gta5" }
+this_is_a_map "yes"
+
+author 'DC Customz'
+description 'DC Customz Smoker Props 1'
+version '1.0.0'
+
+files {
+	'stream/smokerspackone.ytyp'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/smokerspackone.ytyp'
